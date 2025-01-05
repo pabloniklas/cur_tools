3561dd8821621301f485dc6b58afa4da5a09cad5
Pablo Niklas <pablo.niklas@gmail.com>
2025-01-05 18:07:45 -0300

    Carga inicial.

---
+++ modules/constant.py
@@ -0,0 +1,44 @@
+
+STATUSBAR_PREFIX = " StatusBar | "
+
+_pair_pointer = 1
+PAIR_SCREEN_BG = _pair_pointer
+
+_pair_pointer += 1
+PAIR_WINDOW_BG_LOWER = _pair_pointer
+_pair_pointer += 1
+PAIR_WINDOW_BG_UPPER = _pair_pointer
+
+_pair_pointer += 1
+PAIR_WINDOW_TITLE = _pair_pointer
+
+_pair_pointer += 1
+PAIR_INPUT_FIELD = _pair_pointer
+
+_pair_pointer += 1
+PAIR_WINDOW_SHADOW = _pair_pointer
+
+_pair_pointer += 1
+PAIR_WINDOW_HELPER = _pair_pointer
+
+_pair_pointer += 1
+PAIR_ITEM_SELECTED = _pair_pointer
+_pair_pointer += 1
+PAIR_HOTKEY_SELECTED = _pair_pointer
+_pair_pointer += 1
+PAIR_HOTKEY_UNSELECTED = _pair_pointer
+_pair_pointer += 1
+PAIR_ITEM_UNSELECTED = _pair_pointer
+
+_pair_pointer += 1
+PAIR_ERROR_WINDOW = _pair_pointer
+
+# Input types
+INPUT_TYPE_ALPHANUMERIC = 0
+INPUT_TYPE_NUMERIC = 1
+INPUT_TYPE_ALPHABETIC = 2
+
+# ASCI Graphics
+CHAR_LOW_GRAY = "░"
+CHAR_MEDIUM_GRAY = "▒"
+CHAR_HIGH_GRAY = "▓"
